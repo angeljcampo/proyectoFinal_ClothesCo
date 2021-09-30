bdf7b56cacf34b1930933b43e960bd5f5d1bba2d
Angel <angeljcampo@gmail.com>
2021-09-30 14:22:32 -0300

    creating subcategory scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_170018) do
+ActiveRecord::Schema.define(version: 2021_09_30_172133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,13 @@ ActiveRecord::Schema.define(version: 2021_09_30_170018) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
+  end
+
+  add_foreign_key "subcategories", "categories"
 end
