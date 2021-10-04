e832e3f3ac28f6599dff773ed0ea697e2d6bf4c6
Angel <angeljcampo@gmail.com>
2021-10-04 19:33:37 -0300

    creating product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_172133) do
+ActiveRecord::Schema.define(version: 2021_09_30_172638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2021_09_30_172133) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "description"
+    t.integer "price"
+    t.integer "stock"
+    t.bigint "subcategory_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subcategory_id"], name: "index_products_on_subcategory_id"
+  end
+
   create_table "subcategories", force: :cascade do |t|
     t.string "name"
     t.bigint "category_id"
@@ -29,5 +41,6 @@ ActiveRecord::Schema.define(version: 2021_09_30_172133) do
     t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
+  add_foreign_key "products", "subcategories"
   add_foreign_key "subcategories", "categories"
 end
