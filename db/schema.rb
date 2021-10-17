8fc8cabf869a51b982ae5b73bf474f64be20bab9
Angel <angeljcampo@gmail.com>
2021-10-17 17:39:56 -0300

    adding ratyrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_190941) do
+ActiveRecord::Schema.define(version: 2021_10_17_203419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,15 +41,13 @@ ActiveRecord::Schema.define(version: 2021_10_17_190941) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "average_caches", force: :cascade do |t|
-    t.bigint "rater_id"
+  create_table "average_caches", id: :serial, force: :cascade do |t|
+    t.integer "rater_id"
     t.string "rateable_type"
-    t.bigint "rateable_id"
+    t.integer "rateable_id"
     t.float "avg", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["rateable_type", "rateable_id"], name: "index_average_caches_on_rateable_type_and_rateable_id"
-    t.index ["rater_id"], name: "index_average_caches_on_rater_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "carts", force: :cascade do |t|
@@ -78,13 +76,12 @@ ActiveRecord::Schema.define(version: 2021_10_17_190941) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "overall_averages", force: :cascade do |t|
+  create_table "overall_averages", id: :serial, force: :cascade do |t|
     t.string "rateable_type"
-    t.bigint "rateable_id"
+    t.integer "rateable_id"
     t.float "overall_avg", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["rateable_type", "rateable_id"], name: "index_overall_averages_on_rateable_type_and_rateable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -108,16 +105,27 @@ ActiveRecord::Schema.define(version: 2021_10_17_190941) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "rating_caches", force: :cascade do |t|
+  create_table "rates", id: :serial, force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "stars", null: false
+    t.string "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+    t.index ["rater_id"], name: "index_rates_on_rater_id"
+  end
+
+  create_table "rating_caches", id: :serial, force: :cascade do |t|
     t.string "cacheable_type"
-    t.bigint "cacheable_id"
+    t.integer "cacheable_id"
     t.float "avg", null: false
     t.integer "qty", null: false
     t.string "dimension"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
-    t.index ["cacheable_type", "cacheable_id"], name: "index_rating_caches_on_cacheable_type_and_cacheable_id"
   end
 
   create_table "users", force: :cascade do |t|
