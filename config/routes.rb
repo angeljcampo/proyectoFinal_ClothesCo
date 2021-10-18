d91f94bed7b2cace535b9f0aaa3dc6c4bfeb3ff9
Angel <angeljcampo@gmail.com>
2021-10-18 19:34:35 -0300

    fixing heroku app

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :payments
   root 'products#index'
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-    }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :carts, only: [:show, :update, :destroy]
   resources :orders
   resources :products
   resources :categories
+  post '/rate' => 'rater#create', :as => 'rate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
