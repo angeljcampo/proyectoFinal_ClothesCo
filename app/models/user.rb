0d213fef82f5f6149c1f0f855192063e375c29e1
Angel <angeljcampo@gmail.com>
2021-10-21 22:23:00 -0300

    fixing search form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
   has_many :orders, dependent: :destroy
+  has_many :carts, dependent: :destroy
   validates :name, :lastname, :email, :rut, :phone, :address, presence: true
   ratyrate_rater
 
