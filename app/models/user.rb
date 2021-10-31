d01e496cfc1fcb470028259986b1d68a1becf177
Angel <angeljcampo@gmail.com>
2021-10-31 19:56:03 -0300

    fixing heroku omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github google_oauth2]
-  has_many :orders, dependent: :destroy
-  has_many :carts, dependent: :destroy
-  validates :name, :lastname, :email, :rut, :phone, :address, presence: true
-  ratyrate_rater
-
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable,
+  :omniauthable, omniauth_providers: %i[github google_oauth2 ]
+  validates :name, :lastname, :email, :rut, :address, presence: true
   
-  def to_s
-    name
-  end
+  ratyrate_rater
   
   def self.from_omniauth(auth)
-    user = User.find_by(email: auth.info.email)
-    if user
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.save
-    else
-      user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-      end
-    end
-    user
+  user = User.find_by(email: auth.info.email)
+  if user
+  user.provider = auth.provider
+  user.uid = auth.uid
+  user.save
+  else
+  user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  user.email = auth.info.email
+  user.password = Devise.friendly_token[0,20]
   end
-end
+  end
+  user
+  end
+  end
