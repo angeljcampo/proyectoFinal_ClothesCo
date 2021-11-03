8c112e49e295096a84abd1edf66ecb9cfe889290
Angel <angeljcampo@gmail.com>
2021-11-02 22:23:44 -0300

    modifying navbar

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,8 @@
 class StatsController < ApplicationController
   def my_stats
-    @buyers = User.group(:name).count
+    @weekly_buyers = Order.where(status: "completed").group_by_week(:created_at).count
+    @weekly_users = User.group_by_week(:created_at).count
+    @diary_buyers = Order.where(status: "completed").group_by_day(:created_at).count
+    @diary_users = User.group_by_day(:created_at).count
   end
 end
